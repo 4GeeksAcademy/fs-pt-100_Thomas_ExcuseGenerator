365de95942b029a24fea915f181444d75adb6676
timo131 <thomasemosley3@gmail.com>
2025-03-12 11:32:01 +0000

    first draft

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let subjects = ["My friend", "A stranger", "My boss", "A rabid bat"];
+let actions = ["slapped me", "died", "bit me", "fired me", "hurt my feelings"];
+
+const getRandom = (arr) => {
+  let randNum = Math.random();
+  return Math.floor(randNum * arr.length)
+}
+
+const generateExcuse = () => {
+  let randomSubject = subjects[getRandom(subjects)]
+  let randomAction = actions[getRandom(actions)]
+
+  let randomExcuse = `${randomSubject} ${randomAction}.`;
+  document.getElementById("excuse").innerHTML = randomExcuse;
 };
+
+window.onload = generateExcuse;
