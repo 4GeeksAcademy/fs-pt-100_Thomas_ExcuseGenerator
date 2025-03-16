57fb5d69eec5e256e4747c531347e009fa575ca4
timo131 <thomasemosley3@gmail.com>
2025-03-16 19:06:44 +0000

    functions added

--- src/app.js
+++ src/app.js
@@ -4,21 +4,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let subjects = ["My friend", "A stranger", "My boss", "A rabid bat", "Pedro Sanchez", "An ex-convict", "My neighbor's cat", "An alien"];
+let actions = [{verb: "slapped", transitive: true}, {verb: "died", transitive: false}, {verb: "bit", transitive: true}, {verb: "fired", transitive: true}, {verb: "fell into a coma", transitive: false}, {verb: "spontaneously combusted", transitive: false}, {verb: "sneezed on", transitive: true}, {verb: "threw up on", transitive: true}, {verb: "disappeared", transitive: false}, {verb: "ran over", transitive: true}];
+let objects = ["me", "your mom", "my little brother", "my grandma", "my guncle", "Leticia Sabater", "my ex", "a robot"]
 
-let subjects = ["My friend", "A stranger", "My boss", "A rabid bat"];
-let actions = ["slapped me", "died", "bit me", "fired me", "hurt my feelings"];
+const getRandomIndex = arr => Math.floor(Math.random() * arr.length);
 
-const getRandom = (arr) => {
-  let randNum = Math.random();
-  return Math.floor(randNum * arr.length)
-}
+const getSubject = () => subjects[getRandomIndex(subjects)];
+
+const getAction = () => actions[getRandomIndex(actions)];
+
+const getObject = () => objects[getRandomIndex(objects)];
+
+const getPredicate = (actionObj) => {
+  if (actionObj.transitive) {
+    return `${actionObj.verb} ${getObject()}`;
+  } else {
+    return actionObj.verb;
+  }
+};
 
 const generateExcuse = () => {
-  let randomSubject = subjects[getRandom(subjects)]
-  let randomAction = actions[getRandom(actions)]
-
-  let randomExcuse = `${randomSubject} ${randomAction}.`;
-  document.getElementById("excuse").innerHTML = randomExcuse;
+  const subject = getSubject();
+  const actionObj = getAction();
+  const predicate = getPredicate(actionObj);
+  document.getElementById("excuse").innerHTML = `${subject} ${predicate}.`;
 };
 
 window.onload = generateExcuse;
